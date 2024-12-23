f9e6e47e65ab0e279a3c671b94f7343ef1591825
Андросов Юрий <androsov1122@mail.ru>
2024-12-23 21:13:10 +0300

    Создана структура PassengertTansportation

--- Program.cs
+++ Program.cs
@@ -1,14 +1,69 @@
 ﻿
 using System.Drawing;
 
+static int CompareTransportationsBySideNumberOfTheBus(PassengertTansportation a, PassengertTansportation b)
+{
+    return a.sideNumberOfTheBus!.CompareTo(b.sideNumberOfTheBus!);
+}
+
+static void SortTransportationsBySideNumberOfTheBus(PassengertTansportation[] transportations)
+{
+    Array.Sort(transportations, CompareTransportationsBySideNumberOfTheBus);
+    Console.Clear();
+}
+
+static int CompareTransportationsByBrandOfTheBus(PassengertTansportation a, PassengertTansportation b)
+{
+    return a.brandOfTheBus!.CompareTo(b.brandOfTheBus!);
+}
+
+static void SortTransportationsTransportationsByBrandOfTheBus(PassengertTansportation[] transportations)
+{
+    Array.Sort(transportations, CompareTransportationsByBrandOfTheBus);
+    Console.Clear();
+}
+
+static int CompareTransportationsByRouteNumber(PassengertTansportation a, PassengertTansportation b)
+{
+    return a.routeNumber!.CompareTo(b.routeNumber!);
+}
+
+static void SortTransportationsTransportationsByRouteNumber(PassengertTansportation[] transportations)
+{
+    Array.Sort(transportations, CompareTransportationsByRouteNumber);
+    Console.Clear();
+}
+
+static int CompareTransportationsByFullNameOfTheDriver(PassengertTansportation a, PassengertTansportation b)
+{
+    return a.fullNameOfTheDriver!.CompareTo(b.fullNameOfTheDriver!);
+}
+
+static void SortTransportationsTransportationsByFullNameOfTheDriver(PassengertTansportation[] transportations)
+{
+    Array.Sort(transportations, CompareTransportationsByFullNameOfTheDriver);
+    Console.Clear();
+}
+
+static int CompareTransportationsByDateOfWork(PassengertTansportation a, PassengertTansportation b)
+{
+    return a.dateOfWork!.CompareTo(b.dateOfWork!);
+}
+
+static void SortTransportationsTransportationsByDateOfWork(PassengertTansportation[] transportations)
+{
+    Array.Sort(transportations, CompareTransportationsByDateOfWork);
+    Console.Clear();
+}
+
 struct PassengertTansportation
 {
-    int sideNumberOfTheBus;
-    string brandOfTheBus;
-    int routeNumber;
-    string fullNameOfTheDriver;
-    DateOnly dateOfWork;
-    DateOnly startTimeOfWork;
-    DateOnly endTimeOfWork;
-    double revenue;
+    public int sideNumberOfTheBus;
+    public string brandOfTheBus;
+    public int routeNumber;
+    public string fullNameOfTheDriver;
+    public DateOnly dateOfWork;
+    public DateOnly startTimeOfWork;
+    public DateOnly endTimeOfWork;
+    public double revenue;
 }
